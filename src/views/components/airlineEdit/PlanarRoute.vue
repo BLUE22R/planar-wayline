d072215aca652addd38d3c6acfa76a65e40ceb68
CherryBluer <1158851320@qq.com>
2026-08-20 11:30:34 +0800

    fix:挖孔 编辑不启用问题

--- src/views/components/airlineEdit/PlanarRoute.vue
+++ src/views/components/airlineEdit/PlanarRoute.vue
@@ -28,17 +28,24 @@
 
 		<div class="planar-content">
 	<PlanarRouteConfigPanel
-		:fly-point-status="flyPointStatus"
-		:is-oblique-mode="isObliqueMode"
-		:photo-trigger-options="photoTriggerOptions"
-		@change="recalculateRoute"
-		@update-fly-point="updateFlyPoint"
-		@update-climb-type="updateClimbType"
-		@change-line-angle="changeLineAngle"
-		@line-angle-slider-start="beginLineAngleSliderInteraction"
-		@line-angle-slider-input="handleLineAngleSliderInput"
-		@line-angle-slider-change="handleLineAngleSliderChange"
-	/>
+			:fly-point-status="flyPointStatus"
+			:is-oblique-mode="isObliqueMode"
+			:photo-trigger-options="photoTriggerOptions"
+			:has-polygon="hasPolygon"
+			:is-drawing-hole="isDrawingHole"
+			:is-polygon-dirty="isPolygonDirty"
+			:is-calculating="isCalculating"
+			:hole-count="holeCount"
+			@change="recalculateRoute"
+			@update-fly-point="updateFlyPoint"
+			@update-climb-type="updateClimbType"
+			@change-line-angle="changeLineAngle"
+			@line-angle-slider-start="beginLineAngleSliderInteraction"
+			@line-angle-slider-input="handleLineAngleSliderInput"
+			@line-angle-slider-change="handleLineAngleSliderChange"
+			@start-hole-drawing="startHoleDrawing"
+			@generate-route="generateRoute"
+		/>
 			<div class="wayMap planar-map" v-loading="isCalculating || isImporting" :element-loading-text="mapLoadingText">
 				<CesiumMap @loadMap="loadMainMap"></CesiumMap>
 				<div v-if="isObliqueMode && activeRouteSummary" class="route-switcher">
@@ -71,6 +78,7 @@
 					</div>
 				</div>
 				<div class="delBtn" ref="delBtnRef" @click="delPoy">删除测区</div>
+				<div class="delBtn" ref="delHoleBtnRef" @click="deleteHole">删除挖孔</div>
 				<div id="map-error-tip">
 					<el-icon style="transform: translateY(3px)"><InfoFilled /></el-icon>
 					测区不支持交叉面，无法生成航线
